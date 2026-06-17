08c6edeff0d15bb76bc701c22d4a3e118846a5a8
ivanmahnov80-gif <ivanmahnov80@gmail.com>
2026-06-18 00:58:26 +0300

    Add iOS packaging workflow and update website download links

--- website/index.php
+++ website/index.php
@@ -119,6 +119,28 @@ require_once 'lang.php';
                         <line x1="12" y1="15" x2="12" y2="3"></line>
                     </svg>
                 </a>
+                <a href="downloads/tortu-android.apk" class="btn-download">
+                    <div class="download-info">
+                        <span class="download-name"><?php echo $t['download_android']; ?></span>
+                        <span class="download-sub">tortu-android.apk</span>
+                    </div>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                </a>
+                <a href="downloads/tortu-ios.ipa" class="btn-download">
+                    <div class="download-info">
+                        <span class="download-name"><?php echo $t['download_ios']; ?></span>
+                        <span class="download-sub">tortu-ios.ipa</span>
+                    </div>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                </a>
                 <a href="downloads/tortu-1.0.0-Linux.deb" class="btn-download">
                     <div class="download-info">
                         <span class="download-name"><?php echo $t['download_deb']; ?></span>
